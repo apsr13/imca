6171981aaeb2fa32439010ec6f88e44ed97633e6
apsr13 <apoorva.srivastava13@gmail.com>
2016-11-26 16:33:18 -0600

    First commit

---
+++ src/login/login.js
@@ -0,0 +1,27 @@
+import {inject} from 'aurelia-dependency-injection';
+import {Aurelia} from 'aurelia-framework';
+import {Server, User} from 'backend/server';
+
+@inject(Aurelia, Server)
+export class Login {
+  constructor(aurelia, server) {
+    this.aurelia = aurelia;
+    this.server = server;
+    this.username = '';
+    this.password = '';
+    this.message = '';
+  }
+  
+  login() {
+    this.server.login(this.username, this.password).then(result => {
+      if (result) {
+        this.message = '';
+        //TODO: Track the logged in user and render the main app.
+        this.aurelia.use.instance(User, result);
+        this.aurelia.setRoot('shell/shell');
+      } else {
+        this.message = 'Incorrect Username or Password!';
+      }
+    });
+  }
+}
